e8bbbfa21bb01451105639898597103e68602cca
kirananto <kirananto@gmail.com>
2015-07-17 11:16:23 +0530

    Revert "added kernel level Sound Control GPL interface (thanks to @faux123)"
    This reverts commit f529c7b8b12d0462c1f71eae74203ac686d37edf.

--- sound/soc/codecs/wcd9310.c
+++ sound/soc/codecs/wcd9310.c
@@ -8952,10 +8952,7 @@ static const struct file_operations codec_mbhc_debug_ops = {
 	.read = codec_mbhc_debug_read,
 };
 #endif
-#ifdef CONFIG_SOUND_CONTROL_HAX_GPL
-struct snd_kcontrol_new *gpl_faux_snd_controls_ptr =
-		(struct snd_kcontrol_new *)tabla_snd_controls;
-#endif
+
 static int tabla_codec_probe(struct snd_soc_codec *codec)
 {
 	struct wcd9xxx *control;
