e8674bf3dbdfd53ddcbc68943ae80849dba055c3
kirananto <kirananto@gmail.com>
2015-07-18 23:13:37 +0530

    Revert "500111111"
    This reverts commit 3eaebfb24231b2dc4666e70df3e336d55a579341.

--- lettuce.sh
+++ lettuce.sh
@@ -48,7 +48,7 @@ then
 echo -e "$red Kernel Compilation failed! Fix the errors! $nocol"
 exit 1
 fi
-$DTBTOOL -2 -o $KERNEL_DIR/arch/arm64/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/
+$DTBTOOL -2 -o $KERNEL_DIR/arch/arm64/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/
 
 }
 
